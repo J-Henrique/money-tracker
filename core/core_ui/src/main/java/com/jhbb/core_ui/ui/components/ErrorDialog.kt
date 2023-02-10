df57fc68a25aa9ce783e44f5d301b59065d069bb
João Henrique Buso Bortolotto <joao.bortolotto@willbank.com.br>
2023-02-10 15:51:53 -0300

    ErrorDialog.kt

---
+++ core/core_ui/src/main/java/com/jhbb/core_ui/ui/components/ErrorDialog.kt
@@ -0,0 +1,79 @@
+package com.jhbb.core_ui.ui.components
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.rememberLottieComposition
+import com.jhbb.core_ui.R
+import com.jhbb.core_ui.ui.theme.MoneyTrackerTheme
+import com.jhbb.core_ui.utils.MultiThemePreview
+
+@Composable
+fun ErrorDialog(
+    onDismiss: () -> Unit
+) {
+    val composition by rememberLottieComposition(
+        LottieCompositionSpec.RawRes(R.raw.error)
+    )
+    Dialog(onDismissRequest = { onDismiss() }) {
+        Surface(
+            modifier = Modifier.clip(MaterialTheme.shapes.small)
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .fillMaxHeight(0.6f)
+                    .padding(16.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.SpaceBetween
+            ) {
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center
+                ) {
+                    LottieAnimation(
+                        composition = composition,
+                        modifier = Modifier.size(128.dp, 128.dp)
+                    )
+                    Text(
+                        text = stringResource(id = R.string.generic_error_message),
+                        style = MaterialTheme.typography.h3.copy(
+                            fontWeight = FontWeight.Bold
+                        )
+                    )
+                }
+                Button(
+                    onClick = { onDismiss() },
+                    modifier = Modifier.fillMaxWidth(),
+                    colors = ButtonDefaults.buttonColors(
+                        backgroundColor = MaterialTheme.colors.error,
+                        contentColor = contentColorFor(backgroundColor = MaterialTheme.colors.onError)
+                    )
+                ) {
+                    Text(
+                        text = stringResource(id = R.string.dialog_close_button),
+                        fontWeight = FontWeight.Bold
+                    )
+                }
+            }
+        }
+    }
+}
+
+@MultiThemePreview
+@Composable
+fun PreviewErrorDialog() {
+    MoneyTrackerTheme {
+        ErrorDialog {}
+    }
+}
