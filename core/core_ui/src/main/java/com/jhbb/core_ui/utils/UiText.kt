29a27338da5907d95d6e2ff5ca527f9606994f19
João Henrique Buso Bortolotto <joao.bortolotto@willbank.com.br>
2023-02-10 14:59:05 -0300

    UiText.kt

---
+++ core/core_ui/src/main/java/com/jhbb/core_ui/utils/UiText.kt
@@ -0,0 +1,15 @@
+package com.jhbb.core_ui.utils
+
+import android.content.Context
+
+sealed class UiText {
+    data class DynamicString(val text: String): UiText()
+    data class StringResource(val resId: Int): UiText()
+
+    fun toString(context: Context): String {
+        return when (this) {
+            is DynamicString -> text
+            is StringResource -> context.getString(resId)
+        }
+    }
+}
